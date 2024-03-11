9dd7bad5b8981853471f595e0ea293a9d40aed53
Aditya <104072819+Soumya2411@users.noreply.github.com>
2024-03-11 18:30:15 +0530

    contact form update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,7 +6,7 @@ function ContactForm() {
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-950 dark:text-white">Contact Us</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-600 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-                <form action="https://formspree.io/f/mvoeqwkj" className="space-y-8">
+                <form action="https://formspree.io/f/mvoeqwkj" method='POST' className="space-y-8">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-950 dark:text-gray-300">Your email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-950 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-600 dark:focus:border-primary-600 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
